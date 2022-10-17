71812a623c0a456d6f45fc584c04af9add279396
Satyam Sharma <satyamsharma3524@gmail.com>
2022-10-17 20:09:26 +0530

    Adding Search functionality to the message box.

--- static/script.js
+++ static/script.js
@@ -4,6 +4,8 @@ const menuItems = document.querySelectorAll('.menu-item');
 //Messages
 const messagesNotification = document.querySelector('#messages-notifications');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
 // ------------------- Sidebar ------------------- //
 
@@ -22,6 +24,23 @@ menuItems.forEach(item => {
 })
 
 // ------------------- Messages ------------------- //
+//searches chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+//Search Chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// Highlight message box when the message menu item is clicked.
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
@@ -29,3 +48,5 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+
